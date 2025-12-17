4b9c62d16ec63470a9dd1dcb9d103ef3c7388ce8
Alexander1Konstantinov <123snka123@gmail.com>
2025-12-17 21:32:55 +0300

    init_ver

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECRET_KEY = 'django-insecure-f!9+%o#q*ngdl8m*6x3n%e$^kx%^hv=4p#+zsj@r0*l9u4rqg3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
